a4ce5c90c8cc8d2ee8fb07a5208ad693aef71484
Artak <artaktorozyan@gmail.com>
2024-09-09 19:56:27 +0400

    Add deletted files

---
+++ src/PrintCapitalLetter.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class PrintCapitalLetter {
+    public static void main(String[] args) {
+        Scanner Font = new Scanner(System.in);
+        System.out.print( "Input your small letter: ");
+        char smallLetter = Font.next().charAt(0);
+        char capitalLetter = (char) (smallLetter - 32);
+        System.out.println( "Capital letter of your inputted small letter is " + capitalLetter );
+
+    }
+
+}
