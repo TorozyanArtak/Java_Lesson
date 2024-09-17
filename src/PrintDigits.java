a52d79edda2afd179781e6af221ce3e0810a2758
Artak <artaktorozyan@gmail.com>
2024-09-18 03:58:15 +0400

    Changing PrintDigits file

--- src/PrintDigits.java
+++ src/PrintDigits.java
@@ -1,18 +1,29 @@
 // Տպում ենք դրական ամբողջ թվի թվանշանները ձախից աջ հերթականությամբ
+import java.util.Scanner;
+
 public class PrintDigits {
     public static void main(String[] args) {
-        int number = 691257;
-        int length = number;
-        int dozen = 1;
-        while (length >= 10) {
-            length /= 10;
-            dozen *= 10;
+        System.out.println("Input positive number");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Your number is ");
+        int number = scanner.nextInt();
+        if (number <= 0) {
+            System.out.println("Number can not be non positive");
+            System.exit(2);
         }
-        while (dozen > 0) {
-            int digit = number / dozen;
-            System.out.print(digit + " ");
-            number %= dozen;
-            dozen /= 10;
+        int length = 0;
+        int sameNum = number;
+        while (sameNum > 0) {
+            sameNum /= 10;
+            length++;
+        }
+        int[] arrayDigits = new int[length];
+        for (int i = 0 ; i < length; i++) {
+            arrayDigits[length-1-i] = number % 10;
+            number /= 10;
+        }
+        for (int j = 0; j< length; j ++) {
+            System.out.print( arrayDigits[j] + " ");
         }
     }
 }
