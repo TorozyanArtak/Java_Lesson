8c10a6a578b33e4cd143e5027c8461fc150b2739
Artak <artaktorozyan@gmail.com>
2024-09-22 14:28:29 +0400

    Changed solution logic of ArrayPositiveNegativeNumbers.java file

--- src/Homework_21_09_24/ArrayPositiveNegativeNumbers.java
+++ src/Homework_21_09_24/ArrayPositiveNegativeNumbers.java
@@ -3,23 +3,26 @@ package Homework_21_09_24;
 
 public class ArrayPositiveNegativeNumbers {
     public static double getDivisionPosNegNumbers(int[] array) {
-        int sumNegative = 0;
-        int sumPositive = 0;
-        for (int i : array) {
-            if (i > 0) {
-                sumPositive += i;
+        int countNegative = 0;
+        int countPositive = 0;
+        for (int i= 0; i< array.length; i++) {
+            if( array[i] == 0){
+                continue;
+            }
+            if (array[i] > 0) {
+                countPositive ++;
             } else {
-                sumNegative += i;
+                countNegative ++;
             }
         }
-        if (sumNegative == 0) {
+        if (countNegative == 0) {
             return -1;
         }
-        return (double) sumPositive / sumNegative;
+        return (double) countPositive / countNegative;
     }
 
     public static void main(String[] args) {
-        int[] array1 = {2, 8, 14, -10, -21, 80, -2, 100};
+        int[] array1 = {2, 8, 0, -10, -21, 80, -2, 100};
         int[] array2 = {5, 8, 15, 25, 35, 45, 5};
         System.out.println(getDivisionPosNegNumbers(array1));
         System.out.println(getDivisionPosNegNumbers(array2));
